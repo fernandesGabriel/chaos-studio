8be007a9a91f017bdd9039537891233e0aee173c
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2019-06-19 21:57:17 +0200

    Proper setup abstract class

--- chaos/technique/technique.py
+++ chaos/technique/technique.py
@@ -1,10 +1,12 @@
+from abc import ABC, abstractmethod
 from PIL import Image as Canvas
 from trace import Trace
 
 
-class Technique(object):
+class Technique(ABC):
     def __init__(self, trace: Trace):
         self.trace = trace
 
+    @abstractmethod
     def draw_on(self, canvas: Canvas):
         pass
