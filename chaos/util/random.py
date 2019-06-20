2026eafe779b537be21f68e60d90a132bc57fb91
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2019-06-20 10:11:42 +0200

    Simplify random_coordinates

--- chaos/util/random.py
+++ chaos/util/random.py
@@ -18,18 +18,15 @@ def distributed_coordinates(n, x, y, sigma):
 
 
 def random_coordinates(n, x1, y1, sigma1, x2, y2, sigma2):
-    points_1 = distributed_coordinates(n, x1, y1, sigma1)
-    points_2 = distributed_coordinates(n, x2, y2, sigma2)
-
     coordinates = numpy.zeros((n, 2, 2))
 
-    coordinates[:, 0] = points_1
-    coordinates[:, 1] = points_2
+    coordinates[:, 0] = distributed_coordinates(n, x1, y1, sigma1)
+    coordinates[:, 1] = distributed_coordinates(n, x2, y2, sigma2)
 
     response = []
     for i in range(n):
         response.append(
-            [(coordinates[i, 0][0], coordinates[i, 0][1]), (coordinates[i, 1][0], coordinates[i, 1][1])]
+            [(coordinates[i, 0, 0], coordinates[i, 0, 1]), (coordinates[i, 1, 0], coordinates[i, 1, 1])]
         )
 
     return response
